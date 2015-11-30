5226cb2c5f7a8165d3878b9a1d28329dea38bb1c
krnese <kristiannese@live.com>
2015-11-30 19:24:51 +0100

    new ps

---
+++ AzureContainer/Container.ps1
@@ -0,0 +1,23 @@
+﻿Param (
+    [Parameter(Mandatory=$true)]
+    [int]$count
+)
+
+$img = Get-ContainerImage
+$ConNames =@(1..$count)
+
+if ($count -ge 2)
+
+    {
+        Foreach ($Con in $ConNames) 
+
+            {
+                $Con = "WinCon"+$Count 
+                new-container -name $con -ContainerImage $img -SwitchName (Get-VMSwitch).Name -RuntimeType Default -ContainerComputerName $con 
+            }
+    }
+
+else
+    {
+        new-container -name Tp4 -ContainerImage $img -SwitchName (Get-VMSwitch).Name -RuntimeType Default -ContainerComputerName Tp4 
+    }
