1fc0b3334cb3ed6cd6981a4ca407f5fa7a7ff18b
krnese <kristiannese@live.com>
2015-12-08 08:30:44 +0100

    new file

---
+++ AzureContainer/Container.ps1
@@ -0,0 +1,30 @@
+﻿Param (
+    [Parameter(Mandatory=$true)]
+    [int]$count
+)
+
+# Waiting for the Custom Extension to complete before proceeding...
+
+Start-Sleep -Seconds 120
+
+# Ok, let's go!
+
+$img = Get-ContainerImage
+$ConNames =@(1..$count)
+
+if ($count -ge 2)
+
+    {
+        Foreach ($Con in $ConNames) 
+
+            {
+                $Con = "WinCon"+$Con 
+                new-container -name $con -ContainerImage $img -SwitchName (Get-VMSwitch).Name -RuntimeType Default -ContainerComputerName $con 
+            }
+    }
+
+else
+
+    {
+        new-container -name Tp4 -ContainerImage $img -SwitchName (Get-VMSwitch).Name -RuntimeType Default -ContainerComputerName Tp4 
+    }
