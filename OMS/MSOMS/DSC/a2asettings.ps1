741f791bdd2bb72c6d9aff34f967ad1fa56a9611
Kristian Nese <kristiannese@live.com>
2016-06-02 14:55:45 +0200

    settings

---
+++ OMS/MSOMS/DSC/a2asettings.ps1
@@ -0,0 +1,26 @@
+﻿### Setup of A2A Coordinater
+
+param (
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$Admincreds,
+        $OMSResourceGroupName,
+        $OMSRecoveryVaultName      
+      )
+
+# Install AzureRM modules for A2A scenario
+
+Find-module -Name AzureRm.SiteRecovery | Install-Module -Force
+
+Find-Module -Name AzureRm.RecoveryServices | Install-Module -Force
+
+find-module -Name AzureRm.RecoveryServices.Backup | Install-Module -Force
+
+# Login to Azure and download the vault credentials
+
+Login-AzureRmAccount -Credential $Admincreds
+
+$vault = Get-AzureRmRecoveryServicesVault -ResourceGroupName $OMSResourceGroupName -Name $OMSRecoveryVaultName
+
+Get-AzureRmRecoveryServicesVaultSettingsFile -Vault $vault -Path c:\a2a\
+
+# That's it!
