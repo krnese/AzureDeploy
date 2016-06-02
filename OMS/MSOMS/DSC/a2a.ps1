8b642692ffb21041b327336d3876dae0beda71e8
Kristian Nese <kristiannese@live.com>
2016-06-02 11:32:30 +0200

    dsc resource

---
+++ OMS/MSOMS/DSC/a2a.ps1
@@ -0,0 +1,16 @@
+﻿Configuration A2A
+{
+  
+    $A2APackageLocalPath = "C:\A2A\MicrosoftAzureSiteRecoveryUnifiedSetup.exe"
+
+    Import-DscResource -ModuleName xPSDesiredStateConfiguration
+
+    Node localhost {
+
+        xRemoteFile OMSPackage {
+            Uri = "https://aka.ms/unifiedinstaller"
+            DestinationPath = $A2APackageLocalPath
+        }
+        }
+    }
+a2a
