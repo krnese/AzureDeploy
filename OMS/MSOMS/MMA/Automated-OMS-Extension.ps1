8e6ba6d9284f240bbbe3bfc113019e54d490c224
Kristian Nese <kristiannese@live.com>
2016-06-16 11:28:14 +0200

    new runbook

---
+++ OMS/MSOMS/MMA/Automated-OMS-Extension.ps1
@@ -0,0 +1,81 @@
+﻿<#
+.Synopsis
+   Runbook for automated IaaS OMS Extensiond deployment in Azure
+.DESCRIPTION
+   This Runbook will enable OMS Extensions on all Azure IaaS VMs
+#>
+
+
+$credential = Get-AutomationPSCredential -Name 'AzureCredentials'
+$subscriptionId = Get-AutomationVariable -Name 'AzureSubscriptionID'
+$OMSWorkspaceId = Get-AutomationVariable -Name 'OMSWorkspaceId'
+$OMSWorkspaceKey = Get-AutomationVariable -Name 'OMSWorkspaceKey'
+$OMSLogAnalyticsName = Get-AutomationVariable -Name 'OMSLogAnalyticsName'
+$OMSLogAnalyticsResourceGroup = Get-AutomationVariable -Name 'OMSLogAnalyticsResourceGroup'
+
+$ErrorActionPreference = 'Stop'
+
+Try {
+        Login-AzureRmAccount -credential $credential
+        Select-AzureRmSubscription -SubscriptionId $subscriptionId
+
+    }
+
+Catch {
+        $ErrorMessage = 'Login to Azure failed.'
+        $ErrorMessage += " `n"
+        $ErrorMessage += 'Error: '
+        $ErrorMessage += $_
+        Write-Error -Message $ErrorMessage `
+                    -ErrorAction Stop
+      }
+
+Try {
+        $VMs = Get-AzureRmVM
+    }
+
+Catch {
+        $ErrorMessage = 'Failed to retrieve the VMs.'
+        $ErrorMessage += " `n"
+        $ErrorMessage += 'Error: '
+        $ErrorMessage += $_
+        Write-Error -Message $ErrorMessage `
+                    -ErrorAction Stop
+     }
+
+Try {
+			foreach ($vm in $vms)
+				{
+					Get-AzureRmVM -Name $vm.name -ResourceGroupName $vm.ResourceGroupName -ErrorAction Stop
+
+            			$IsExt=$null 
+						$IsExt=$vm.Extensions | Where-Object {$_.Publisher -eq "Microsoft.EnterpriseCloud.Monitoring"} 
+						if ($isExt) {Write-output "It's already here!"}
+						
+						else
+
+            			{
+                				foreach ($vm in $vms)
+
+                		{
+                    		New-AzureRmResourceGroupDeployment -Name MMAdeployment `
+                                                               -ResourceGroupName $vm.ResourceGroupName `
+                                                               -TemplateUri $templateuri `
+                                                               -vmName $vm.Name `
+                                                               -vmResourceGroupName $vm.ResourceGroupName `
+                                                               -OMSLogAnalyticsWorkspaceName $OMSLogAnalyticsName `
+                                                               -OMSLogAnalyticsResourceGroup $OMSLogAnalyticsResourceGroup `
+                                                               -Verbose                                                               
+	            		}
+            		}
+				}
+	}
+
+Catch {
+        $ErrorMessage = 'Failed to deploy extensions.'
+        $ErrorMessage += " `n"
+        $ErrorMessage += 'Error: '
+        $ErrorMessage += $_
+        Write-Error -Message $ErrorMessage `
+                    -ErrorAction Stop
+     } 
