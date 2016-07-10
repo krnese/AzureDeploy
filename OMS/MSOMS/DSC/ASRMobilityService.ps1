4720c532b433570e3d059d3bb2520e06b4415c6c
Kristian Nese <kristiannese@live.com>
2016-07-10 13:11:22 +0200

    new script

---
+++ OMS/MSOMS/DSC/ASRMobilityService.ps1
@@ -0,0 +1,75 @@
+﻿configuration ASRMobilityService {
+    
+    $RemoteFile = Get-AutomationVariable -Name "RemoteFile"
+    $RemotePassphrase = Get-AutomationVariable -Name "RemotePassphrase"
+    $TempDestination = "C:\Temp\asr.zip"
+    $LocalPassphrase = "C:\Temp\Mobility_service\passphrase.txt"
+    $Role = 'Agent'
+    $Install = 'C:\Program Files (x86)\Microsoft Azure Site Recovery'
+    $CSEndpoint = Get-AutomationVariable -Name "CSEndpoint"
+    $Passphrase = 'C:\Temp\Moblity_service\passphrase.txt'
+    $Arguments = '/Role ' + '"' + $Role + '"' + ' /InstallLocation ' + '"' + $Install + '"' + ' /CSEndpoint ' + '"' + $CSEndpoint + '"' + ' /PassphraseFilePath ' +  '"' +$Passphrase + '"'  
+
+    Import-DscResource -ModuleName xPSDesiredStateConfiguration
+
+    node localhost {
+        
+        xRemoteFile Setup {
+           URI = $RemoteFile
+           DestinationPath = $TempDestination
+           DependsOn = "[File]Directory"
+            }
+
+        xRemoteFile Passphrase {
+            URI = $RemotePassphrase
+            DestinationPath = $LocalPassphrase
+            DependsOn = "[File]Directory"
+            }
+
+        File Directory {
+            DestinationPath = "C:\Temp\ASRSetup\"
+            Type = "Directory"            
+            }
+
+        Archive ASRzip {
+           Path = $TempDestination
+           Destination = "C:\Temp\ASRSetup"
+           DependsOn = "[xRemotefile]Setup"
+           }
+
+        Package Install {
+            Path = "C:\temp\ASRSetup\ASR\UNIFIEDAGENT.EXE"
+            Ensure = "Present"
+            Name = "Microsoft Azure Site Recovery mobility Service/Master Target Server"
+            ProductId = "275197FC-14FD-4560-A5EB-38217F80CBD1"
+            Arguments = $Arguments
+            DependsOn = "[Archive]ASRzip"
+            }           
+
+        Service ASRvx {
+            Name = "svagents"
+            Ensure = "Present"
+            State = "Running"
+            DependsOn = "[Package]Install"
+            }
+
+        Service ASR {
+            Name = "InMage Scout Application Service"
+            Ensure = "Present"
+            State = "Running"
+            DependsOn = "[Package]Install"
+            }
+        }
+    }
+
+Import-AzureRmAutomationDscConfiguration `
+                                        -AutomationAccountName KNOMSAA `
+                                        -ResourceGroupName KNOMS `
+                                        -SourcePath C:\ASR\ASRMobilityService.ps1 `
+                                        -Published `
+                                        -Description "DSC Config for Mobility Service"
+
+Start-AzureRmAutomationDscCompilationJob `
+                                        -AutomationAccountName KNOMSAA `
+                                        -ResourceGroupName KNOMS `
+                                        -ConfigurationName ASRMobilityService
