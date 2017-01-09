0e4d1f96db0d2d8e4907c82c2955f66f10b974e4
Kristian Nese <kristiannese@live.com>
2017-01-09 14:50:57 +0100

    update

--- OMS/MSOMS/Solutions/custom/scripts/azureMgmtAnalytics.ps1
+++ OMS/MSOMS/Solutions/custom/scripts/azureMgmtAnalytics.ps1
@@ -23,55 +23,57 @@ $OMSAutomationAccountName = Get-AutomationVariable -Name 'OMSAutomationAccountNa
 
 # Fetching all unmanaged VMs
 
-$VMs = Get-AzureRmVm | where-object {$_.Extensions[0] -eq $null}
+$VMs = Get-AzureRmVm 
 
 foreach ($VM in $VMs)
 {
+       
+        if ($vm.AvailabilitySetReference -ne $null)
+        {
+            $HA = 'Highly Available'
+        }
+        else
+        {
+            $HA = 'Not Highly Available'
+        }
 
-    $VMTable = @()
-                $VMData = New-Object psobject -Property @{
-                    VMName = $VM.Name;
-                    ResourceGroupName = $VM.ResourceGroupName;
-                    Location = $VM.Location;
-                    RecommendedActions = 'This VM should be managed by Azure OMS services';
-                    ManagedStatus = 'Unmanaged';
-                    SubscriptionId = $AzureSubscriptionId;
-                    Log = 'IaaS'
-                   }
-    $VMTable += $VMData
-   
-    $VMTableJson = ConvertTo-Json -InputObject $VMData
-    
-    $LogType = 'AzureManagement'
-
-    Send-OMSAPIIngestionData -customerId $omsworkspaceId -sharedKey $omsworkspaceKey -body $VMTableJson -logType $LogType
-
-    }
-
-$VMs = Get-AzureRmVm | where-object {$_.Extensions[0] -ne $null}
-
-foreach ($VM in $VMs)
-{
-        $Extensions = (Get-AzureRmVm -ResourceGroupName $vm.ResourceGroupName -Name $vm.Name).Extensions.id
-
-        $ExtensionTable = @()
-                           $ExtensionData = New-Object psobject -Property @{
-                               VMName = $vm.Name;
-                               ResourceGroupName = $vm.ResourceGroupName;
-                               Location = $vm.Location;
-                               Extension = $Extensions;
-                               ManagedStatus = 'Managed';
-                               SubscriptionId = $AzureSubscriptionId;
-                               Log = 'IaaS'
-                               }
+       $Extensions = Get-AzureRmVm -Name $vm.name -ResourceGroupName $VM.ResourceGroupName | select Extensions
+ 
+       $MMA = 'Microsoft.EnterpriseCloud.Monitoring'
+       $Extensions | ForEach-Object -Process {
+           if ($Extensions.Extensions.Publisher -contains $MMA) 
+           {
+              $Status = "Managed"
+              $Action = "This VM is managed by a Log Analytics Workspace"
+           } 
+           else 
+           {
+              $Status = "Unmanaged" 
+              $Action = "This VM is not managed by a Log Analytics Workspace. You can add the extension to the VM from within the Log Analytics resource in Azure portal, or use tooling like PowerShell and ARM templates to deploy at scale."          
+           }
+                $ExtensionTable = @()
+                $ExtensionData = New-Object psobject -Property @{
+                   VMName = $VM.Name;
+                   ResourceGroupName = $VM.ResourceGroupName;
+                   Location = $VM.Location;
+                 # Extension = $Extensions;
+                   MgmtStatus = $Status;
+                   SubscriptionId = $AzureSubscriptionId;
+                   Log = 'IaaS';
+                   AvailabilityStatus = $HA;
+                   ResourceId = $VM.Id;
+                   RecommendedActions = $Action
+                  }
         $ExtensionTable += $ExtensionData
 
         $ExtensionsTableJson = ConvertTo-Json -InputObject $ExtensionTable
 
+        write-output $extensionstablejson 
+
         $LogType = 'AzureManagement'
 
-        Send-OMSAPIIngestionData -customerId $omsworkspaceId -sharedKey $omsworkspaceKey -body $ExtensionsTableJson -logType $LogType
-
+        Send-OMSAPIIngestionData -customerId $omsworkspaceId -sharedKey $omsworkspaceKey -body $ExtensionsTableJson -logType $LogType                  
+        }
 }
 
 # Finding Automation Accounts
@@ -88,11 +90,11 @@ foreach ($Automation in $AutomationAccounts)
                     ResourceName = $Automation.Name;
                     ResourceGroupName = $Automation.ResourceGroupName;
                     Location = $Automation.Location;
-                    RecommendedActions = 'This PaaS resource should be managed by Azure OMS services';
-                    ManagedStatus = 'Unmanaged'
+                    MgmtStatus = 'Unmanaged'
                     ResourceId = $Automation.ResourceId;
                     ResourceType = $Automation.ResourceType;
-                    Log = 'PaaS'
+                    Log = 'PaaS';
+                    RecommendedActions = "This resource is not managed by a Log Analytics Workspace. Use the Azure portal, PowerShell or ARM templates to enable metrics and diagnostics to Log Analytics so that you can perform forensics and management on your resource"
                    }
     $PaaSTable += $PaaSData
    
@@ -109,8 +111,8 @@ foreach ($Automation in $AutomationAccounts)
                     ResourceName = $Automation.Name;
                     ResourceGroupName = $Automation.ResourceGroupName;
                     Location = $Automation.Location;
-                    RecommendedActions = 'This PaaS resource is managed by OMS Services';
-                    ManagedStatus = 'Managed'
+                    RecommendedActions = 'This resource is managed by a Log Analytics Workspace and you can explore, perform forensics and analytics on the data by using specific solutions or the search engine';
+                    MgmtStatus = 'Managed'
                     ResourceId = $Automation.ResourceId;
                     ResourceType = $Automation.ResourceType;
                     WorkspaceId = $Diagnostics.WorkspaceId;
@@ -142,7 +144,7 @@ foreach ($NSG in $NSGS)
                     ResourceGroupName = $NSG.ResourceGroupName;
                     Location = $NSG.Location;
                     RecommendedActions = 'This PaaS resource should be managed by Azure OMS services';
-                    ManagedStatus = 'Unmanaged'
+                    MgmtStatus = 'Unmanaged'
                     ResourceId = $NSG.ResourceId;
                     ResourceType = $NSG.ResourceType;
                     Log = 'PaaS'
@@ -163,7 +165,7 @@ foreach ($NSG in $NSGS)
                     ResourceGroupName = $NSG.ResourceGroupName;
                     Location = $VM.Location;
                     RecommendedActions = 'This PaaS resource is managed by OMS Services';
-                    ManagedStatus = 'Managed'
+                    MgmtStatus = 'Managed'
                     ResourceId = $NSG.ResourceId;
                     ResourceType = $NSG.ResourceType;
                     WorkspaceId = $Diagnostics.WorkspaceId;
@@ -181,56 +183,305 @@ foreach ($NSG in $NSGS)
 
 # Finding protected and unprotected VMs
 
-$RsVault = Get-AzureRmRecoveryServicesVault -Name $OMSRecoveryVault -ResourceGroupName $OMSResourceGroupName
-Set-AzureRmRecoveryServicesVaultContext -Vault $RsVault
-$AzureVMs = Get-AzureRmRecoveryServicesBackupContainer -ContainerType AzureVM
-Get-AzureRmVM | ForEach-Object -Process {
-    if ($AzureVMs.FriendlyName -contains $_.Name) {
-        $Protected = "Protected"
-    } else {
-        $Protected = "Unprotected"
-    }
-    $VMTable = @()
-       $VMData = New-Object psobject -Property @{
-        VMName = $_.Name;
-        ProtectionStatus = $Protected;
-        Log = 'Backup'
+$backupVaults = Find-AzureRmResource -ResourceType Microsoft.RecoveryServices/vaults
 
-    }
-    $VMTable += $VMData
+foreach ($backupvault in $backupVaults)
+{
+
+        $rsVault = Get-AzureRmRecoveryServicesVault -Name $backupVault.Name -ResourceGroupName $backupVault.ResourceGroupName
+        Set-AzureRmRecoveryServicesVaultContext -Vault $rsVault
+        $azureVMs = Get-AzureRmRecoveryServicesBackupContainer -ContainerType AzureVM
+        Get-AzureRmVM | where-object {$_.Location -eq $rsVault.Location} | ForEach-Object -Process {
+            if ($azureVMs.FriendlyName -contains $_.Name) {
+                $protected = "Protected"
+            } else {
+                $protected = "Unprotected"
+            }
+            $backupTable = @()
+               $backupData = New-Object psobject -Property @{
+                VMName = $_.Name;
+                ProtectionStatus = $protected;
+                LogType = 'Backup';
+                VaultName = $rsVault.Name;
+                Location = $rsVault.Location
+                ResourceGroupName = $_.ResourceGroupName;
+                VMId = $_.Id
+            }
+            $backupTable += $backupData
    
-    $VMTableJson = ConvertTo-Json -InputObject $VMData
+            $backupTableJson = ConvertTo-Json -InputObject $backupData
+
+            write-output $backuptablejson
+            }
+       
+
     $LogType = 'AzureManagement'
 
-    Send-OMSAPIIngestionData -customerId $omsworkspaceId -sharedKey $omsworkspaceKey -body $VMTableJson -logType $LogType
+    Send-OMSAPIIngestionData -customerId $omsworkspaceId -sharedKey $omsworkspaceKey -body $backupTableJson -logType $LogType
     
 }
 
 # Find DSC nodes
 
-$DSCManaged = Get-AzureRmAutomationDscNode -AutomationAccountName $OMSAutomationAccountName -ResourceGroupName $OMSResourceGroupName
+$Auto = Find-AzureRmResource -resourceType Microsoft.Automation/automationAccounts
 
-foreach ($DSC in $DSCManaged)
+foreach ($Au in $Auto)
 {
-        $DSCTable = @()
-                $DSCData = New-Object psobject -Property @{
-                    VMName = $DSC.Name;
-                    ResourceGroupName = $DSC.ResourceGroupName;
-                    NodeConfigurationName = $DSC.NodeConfigurationName;
-                    ManagedStatus = 'Managed'
-                    AutomationAccountName = $DSC.AutomationAccountName;
-                    Status = $DSC.Status;
-                    Log = 'DSC'
-                   }
-    $DSCTable += $DSCData
-   
-    $DSCTableJson = ConvertTo-Json -InputObject $DSCTable
-    
-    $LogType = 'AzureManagement'
 
-    Send-OMSAPIIngestionData -customerId $omsworkspaceId -sharedKey $omsworkspaceKey -body $DSCTableJson -logType $LogType
+$DSCManaged = Get-AzureRmAutomationDscNode -AutomationAccountName $Au.Name -ResourceGroupName $Au.ResourceGroupName
+
+    foreach ($DSC in $DSCManaged)
+    {
+            $DSCTable = @()
+                    $DSCData = New-Object psobject -Property @{
+                        VMName = $DSC.Name;
+                        ResourceGroupName = $DSC.ResourceGroupName;
+                        NodeConfigurationName = $DSC.NodeConfigurationName;
+                        MgmtStatus = 'Managed'
+                        AutomationAccountName = $DSC.AutomationAccountName;
+                        Status = $DSC.Status;
+                        Log = 'DSC'
+                       }
+        $DSCTable += $DSCData
+   
+        $DSCTableJson = ConvertTo-Json -InputObject $DSCTable
+    
+        $LogType = 'AzureManagement'
+
+        Send-OMSAPIIngestionData -customerId $omsworkspaceId -sharedKey $omsworkspaceKey -body $DSCTableJson -logType $LogType
+    }
 }
 
+$omsworkspaceId = '88e919fd-7857-4d95-b706-3ca6fef01279'
+$omsworkspaceKey = 'gfzYBcZfX3fVWLULP0mUlc9x0j+/gbEvR/ukIBQOtEkj8FUF1wNfX2shfC/PF9Bdw5QLHk2qMLwnYE7Xm082Og=='   
 
+# Getting an overview of all existing Azure resources
     
+$Resources = Get-AzureRmResource 
+
+foreach ($resource in $resources)
+{
+       $ResourcesTable = @()
+       $ResourcesData = new-object psobject -Property @{
+           ResourceType = $Resource.ResourceType;
+           ResourceGroupName = $Resource.ResourceGroupName;
+           Location = $Resource.Location;
+           ResourceName = $Resource.Name;
+           ResourceId = $Resource.ResourceId;
+           SubscriptionId = $Resource.SubscriptionId;
+           Log = 'Resources'
+           }
+
+       $ResourcesTable += $ResourcesData
+    
+       $ResourcesJson = ConvertTo-Json -InputObject $ResourcesTable
+
+       Write-Output $ResourcesJson 
+
+       $LogType = 'AzureManagement'
+
+       Send-OMSAPIIngestionData -customerId $omsworkspaceid -sharedKey $omsworkspaceKey -body $ResourcesJson -logType $LogType
+}
                       
+# Getting an overview of all ARM Deployments
+
+$ResourceGroups = Get-AzureRmResourceGroup
+
+foreach ($resourcegroup in $ResourceGroups)
+
+{
+    $Deployments = Get-AzureRmResourceGroupDeployment -ResourceGroupName $resourcegroup.ResourceGroupName
+
+    foreach ($Deployment in $Deployments)
+    {
+        $DeploymentTable = @()
+        $DeploymentData = new-object psobject -Property @{
+            ResourceGroupName = $Deployment.ResourceGroupName;
+            DeploymentName = $Deployment.DeploymentName;
+            ProvisioningState = $Deployment.ProvisioningState;
+            TimeStamp = $Deployment.TimeStamp.ToUniversalTime().ToString('yyyy-MM-ddtHH:mm:ss');
+            Mode = $Deployment.Mode.ToString();
+            Parameters = $deployment.Parameters.Keys + $deployment.Parameters.Values;
+            Outputs = $Deployment.Outputs.Keys + $deployment.Outputs.Values;
+            TemplateLink = $Deployment.TemplateLink;
+            CorrelationId = $Deployment.CorrelationId.ToString()
+            Log = 'Deployments'
+            }
+
+        $DeploymentTable += $DeploymentData
+
+        $DeploymentsJson = ConvertTo-Json -inputobject $deploymenttable
+
+        #write-output $DeploymentsJson
+
+        $LogType = 'AzureManagement'
+
+        Send-OMSAPIIngestionData -customerId $omsworkspaceId -sharedKey $omsworkspaceKey -body $DeploymentsJson -logType $LogType
+    }
+}
+
+$StorageAccounts = Get-AzureRmStorageAccount
+
+foreach ($StorageAccount in $StorageAccounts)
+{
+   if ($StorageAccount.sku.name -eq 'StandardLRS')
+   {
+    $HA = "Not Highly Available"
+   }
+   else
+   {
+    $HA = "Highly Available"
+   }
+
+    $StorageTable = @()
+    $StorageData = new-object psobject -Property @{
+        ResourceGroupName = $StorageAccount.ResourceGroupName;
+        Location = $StorageAccount.Location;
+        AvailabilityStatus = $HA;
+        Sku = $StorageAccount.Sku.Name.ToString();
+        PrimaryLocation = $StorageAccount.PrimaryLocation;
+        #SecondaryLocation = $StorageAccount.SecondaryLocation;
+        StatusOfPrimary = $StorageAccount.StatusOfPrimary;
+        CreationTime = $StorageAccount.CreationTime.ToUniversalTime().ToString('yyyy-MM-ddtHH:mm:ss');
+        #LastGeoFailoverTime = $StorageAccount.LastGeoFailoverTime.ToUniversalTime().ToString('yyyy-MM-ddtHH:mm:ss');
+        ResourceId = $StorageAccount.Id;
+        StorageAccountName = $StorageAccount.StorageAccountName;
+        Log = "Storage";
+        SubscriptionId = $AzureSubscriptionId;
+        Tags = $StorageAccount.Tags
+        }
+    $StorageTable += $StorageData
+
+    $StorageJson = ConvertTo-Json -inputobject $StorageTable
+
+    Write-Output $StorageJson
+
+    $LogType = 'AzureManagement'
+
+    Send-OMSAPIIngestionData -customerId $omsworkspaceId -sharedKey $omsworkspaceKey -body $StorageJson -logType $LogType
+}          
+
+$regions = Get-AzureRmLocation
+foreach ($region in $regions)
+{
+    $VMUsageTotal = Get-AzureRmVMUsage -Location $region.Location
+    
+    foreach ($VMUsage in $VMUsageTotal)
+    {
+        $VMUsageTable = @()
+        $VMUsageData = new-object psobject -property @{
+            Region = $Region.Location;
+            Name = $VMUsage.Name.Value;
+            CurrentValue = $VMUsage.CurrentValue;
+            Limit = $VMUsage.Limit;
+            Log = 'VMUsage'
+            }
+        $VMUsageTable += $VMUsageData
+
+        $VMUsageJSON = ConvertTo-Json -inputobject $VMUsageTable
+
+        write-output $VMUsageJson
+
+    $LogType = 'AzureManagement'
+
+    Send-OMSAPIIngestionData -customerId $omsworkspaceId -sharedKey $omsworkspaceKey -body $VMUsageJSON -logType $LogType
+    }
+}  
+
+# Checking for Policy Assignments
+
+$Policies = Get-AzureRmPolicyAssignment
+
+foreach ($Pol in $Policies)
+    {
+    $PolicyTable = @()
+    $PolicyData = new-object psobject -property @{
+        Name = $Pol.Name;
+        ResourceId = $pol.ResourceId;
+        ResourceType = $pol.ResourceType;
+        SubscriptionId = $AzureSubscriptionId;
+        Properties = $pol.properties;
+        PolicyAssignmentId = $pol.PolicyAssignmentId;
+        ResourceGroupName = $pol.ResourceGroupName;
+        Log = 'Policy'
+        }
+    
+    $PolicyTable += $PolicyData
+
+    $PolicyJson = ConvertTo-Json -inputobject $PolicyTable
+
+    $LogType = 'AzureManagement'
+
+    Write-Output $policyjson
+    Send-OMSAPIIngestionData -customerId $omsworkspaceId -sharedKey $omsworkspaceKey -body $PolicyJson -logType $LogType
+    }
+
+$Tags = Get-AzureRmTag
+
+foreach ($Tag in $Tags)
+{
+    $Value = Get-AzureRmTag -Name $Tag.Name | select -ExpandProperty Values
+
+    $TagTable = @()
+    $TagData = New-Object psobject -property @{
+        TagName = $Tag.Name;
+        Value = $Value.Name;
+        Log = 'Tag'
+        }
+    $TagTable += $TagData
+
+    $TagJson = ConvertTo-Json -inputobject $TagTable
+
+    $LogType = 'AzureManagement'
+
+    Send-OMSAPIIngestionData -customerId $OMSWorkspaceId -sharedKey $OMSWorkspaceKey -body $TagJson -logType $LogType
+    }
+
+$RoleAssignments = Get-AzureRmRoleAssignment
+foreach ($role in $RoleAssignments)
+{
+    $RoleTable = @()
+    $RoleData = new-object psobject -property @{
+        RoleAssignmentId = $Role.RoleAssignmentId;
+        Scope = $Role.Scope;
+        DisplayName = $Role.DisplayName;
+        SignInName = $Role.SignInName;
+        RoleDefinitionName = $Role.RoleDefinitionName;
+        RoleDefinitionId = $Role.RoleDefinitionId;
+        ObjectId = $Role.ObjectId;
+        ObjectType = $Role.ObjectId;
+        Log = 'RBAC';
+        SubscriptionId = $AzureSubscriptionId
+        }
+    $RoleTable += $RoleData
+
+    $RoleJson = ConvertTo-Json -inputobject $RoleTable
+
+    $LogType = 'AzureManagement'
+
+    Send-OMSAPIIngestionData -customerId $omsworkspaceId -sharedKey $omsworkspaceKey -body $RoleJson -logType $LogType
+    }
+
+$locks = Get-AzureRmResourceLock
+
+foreach ($lock in $locks)
+{
+    $LockTable = @()
+    $LockData = new-object psobject -property @{
+        Name = $lock.Name;
+        Log = 'Lock';
+        ResourceName = $lock.ResourceName;
+        ResourceType = $lock.ResourceType;
+        SubscriptionId = $AzureSubscriptionId;
+        LockId = $lock.LockId;
+        ResourceId = $lock.ResourceId;
+        ExtensionResourceType = $lock.ExtensionResourceType;
+        Properties = $lock.properties
+        }
+       $LockTable += $LockData
+       $LockJson = ConvertTo-Json -inputobject $locktable
+       
+       $LogType = 'AzureManagement'
+       
+       Send-OMSAPIIngestionData -customerId $omsworkspaceId -sharedKey $OMSWorkspaceKey -body $LockJson -logType $LogType    
+}
