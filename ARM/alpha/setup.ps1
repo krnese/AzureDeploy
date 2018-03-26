5c8e92e1e13a7ab70d26ae6956b048fef8e2029a
Kristian Nese <kristiannese@live.com>
2018-03-26 23:53:33 +0200

    fix

--- ARM/alpha/setup.ps1
+++ ARM/alpha/setup.ps1
@@ -1,9 +1,5 @@
 # Installing Docker
 
-Install-Module -Name DockerMsftProvider -Repository PSGallery -Force -SkipPublisherCheck
-
-Install-package -Name docker -providername DockerMsftProvider -Confirm:$false -Force -Verbose
-
 # Pulling docker image - this will take some time
 
 docker pull dliaodocker/sqlservr
