e1677ea3d2642dada7cfa9b71996cdf94230817f
Kristian Nese <kristiannese@live.com>
2019-08-26 19:01:05 +0200

    added policy at scale demo snippet

--- ARM/deployments/lighthouseDemo.ps1
+++ ARM/deployments/lighthouseDemo.ps1
@@ -1,38 +1,9 @@
-# Query all tenants
-
-$currentContext = Get-AzContext
-$token = $currentContext.TokenCache.ReadItems() | ? {$_.tenantid -eq $currentContext.Tenant.Id}
-
-# ARM Request
-    $ARMRequest = @{
-        Uri = "https://management.azure.com/tenants?api-version=2019-03-01&includeAllTenantCategories=true"
-        Headers = @{
-            Authorization = "Bearer $($token.AccessToken)"
-            'Content-Type' = 'application/json'
-        }
-        Method = 'Get'
-    }
-    $Query = Invoke-WebRequest @ARMRequest
-    #prettify
-    [Newtonsoft.Json.Linq.JObject]::Parse($Query.Content).ToString()
-
-    $ARMRequest2 = @{
-        Uri = "https://management.azure.com/subscriptions/74903176-3e4e-492d-9d18-9afab69a0cf8/providers/Microsoft.ManagedServices/registrationDefinitions?api-version=2019-06-01"
-        Headers = @{
-            Authorization = "Bearer $($token.AccessToken)"
-            'Content-Type' = 'application/json'
-        }
-        Method = 'Get'
-    }
-    $Query2 = Invoke-WebRequest @ARMRequest2
-    #prettify
-    [Newtonsoft.Json.Linq.JObject]::Parse($Query2.Content).ToString()
 
 # Using Resource Graph to detect storage accounts not being secured by https
 
-Search-AzGraph -Query "summarize count() by tenantId" | ConvertTo-Json
+Search-AzGraph -Query "summarize count() by tenantId" -Subscription (Get-AzSubscription).SubscriptionId | ConvertTo-Json
 
-Search-AzGraph -Query "where type =~ 'Microsoft.Storage/storageAccounts' | project name, location, subscriptionId, tenantId, properties.supportsHttpsTrafficOnly" | convertto-json
+Search-AzGraph -Query "where type =~ 'Microsoft.Storage/storageAccounts' | project name, location, subscriptionId, tenantId, properties.supportsHttpsTrafficOnly" -Subscription (Get-AzSubscription).SubscriptionId | convertto-json
 
 # Deploying Azure Policy using ARM templates at scale across multiple customer scopes, to deny creation of storage accounts not using https
 
@@ -59,7 +30,7 @@ New-AzStorageAccount -ResourceGroupName (New-AzResourceGroup -name kntest -Locat
                      -SkuName Standard_LRS `
                      -Verbose
                      
-# clean-up
+# clean-up the above steps
 
 foreach ($sub in $subs)
 {
