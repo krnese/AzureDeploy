afb83fcfbcded2e7c700e9434f50776885e03fa3
Kristian Nese <kristiannese@live.com>
2019-08-26 19:04:07 +0200

    added demo script for policy deployment at scale

--- ARM/deployments/lighthouseDemo.ps1
+++ ARM/deployments/lighthouseDemo.ps1
@@ -1,20 +1,13 @@
-
 # Using Resource Graph to detect storage accounts not being secured by https
-
 Search-AzGraph -Query "summarize count() by tenantId" -Subscription (Get-AzSubscription).SubscriptionId | ConvertTo-Json
-
 Search-AzGraph -Query "where type =~ 'Microsoft.Storage/storageAccounts' | project name, location, subscriptionId, tenantId, properties.supportsHttpsTrafficOnly" -Subscription (Get-AzSubscription).SubscriptionId | convertto-json
 
 # Deploying Azure Policy using ARM templates at scale across multiple customer scopes, to deny creation of storage accounts not using https
-
 $subs = Get-AzSubscription
-
 Write-Output "In total, there's $($subs.Count) projected customer subscriptions to be managed"
-
 foreach ($sub in $subs)
 {
     Select-AzSubscription -SubscriptionId $sub.id
-
     New-AzDeployment -Name mgmt `
                      -Location eastus `
                      -TemplateUri "https://raw.githubusercontent.com/krnese/AzureDeploy/master/ARM/deployments/PCI/Enforce-HTTPS-Storage-DENY.json" `
@@ -22,7 +15,6 @@ foreach ($sub in $subs)
 }
 
 # Validating the policy - deny creation of storage accounts that are NOT using https only
-
 New-AzStorageAccount -ResourceGroupName (New-AzResourceGroup -name kntest -Location eastus -Force).ResourceGroupName `
                      -Name (get-random) `
                      -Location eastus `
@@ -31,15 +23,11 @@ New-AzStorageAccount -ResourceGroupName (New-AzResourceGroup -name kntest -Locat
                      -Verbose
                      
 # clean-up the above steps
-
 foreach ($sub in $subs)
 {
-    select-azsubscription -subscriptionId $sub.id
-
+    Select-AzSubscription -subscriptionId $sub.id
     Remove-AzDeployment -Name mgmt -AsJob
-
     $Assignment = Get-AzPolicyAssignment | where-object {$_.Name -like "enforce-https-storage-assignment"}
-
     if ([string]::IsNullOrEmpty($Assignment))
     {
         Write-Output "Nothing to clean up - we're done"
@@ -47,7 +35,6 @@ foreach ($sub in $subs)
     else
     {
     Remove-AzPolicyAssignment -Name 'enforce-https-storage-assignment' -Scope "/subscriptions/$($sub.id)" -Verbose
-
     Write-Output "ARM deployment has been deleted - we're done"
     }
 }
